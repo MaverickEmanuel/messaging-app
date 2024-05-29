b9e0ee27bd5cbcd760e2adc421455933896a9fd2
maverickemanuel <mavjamesemanuel@gmail.com>
2024-05-29 14:34:29 -0400

    added functionality to switch between channels

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -69,7 +69,7 @@ const ChannelListContent = ({ isCreating, setIsCreating, setCreateType, setIsEdi
                             type='team'
                             isCreating={isCreating}
                             setIsCreating={setIsCreating}
-                            setCreateType={setCreateType}
+                            setCreateType={setCreateType} 
                             setIsEditing={setIsEditing}
                             setToggleContainer={setToggleContainer}
                         />
@@ -77,6 +77,8 @@ const ChannelListContent = ({ isCreating, setIsCreating, setCreateType, setIsEdi
                     Preview={(previewProps) => (
                         <TeamChannelPreview 
                             {...previewProps}
+                            setIsCreating={setIsCreating}
+                            setIsEditing={setIsEditing}
                             setToggleContainer={setToggleContainer}
                             type='team'
                         />
@@ -91,7 +93,7 @@ const ChannelListContent = ({ isCreating, setIsCreating, setCreateType, setIsEdi
                             type='messaging'
                             isCreating={isCreating}
                             setIsCreating={setIsCreating}
-                            setCreateType={setCreateType}
+                            setCreateType={setCreateType} 
                             setIsEditing={setIsEditing}
                             setToggleContainer={setToggleContainer}
                         />
@@ -99,6 +101,8 @@ const ChannelListContent = ({ isCreating, setIsCreating, setCreateType, setIsEdi
                     Preview={(previewProps) => (
                         <TeamChannelPreview 
                             {...previewProps}
+                            setIsCreating={setIsCreating}
+                            setIsEditing={setIsEditing}
                             setToggleContainer={setToggleContainer}
                             type='messaging'
                         />
@@ -114,24 +118,23 @@ const ChannelListContainer = ({ setCreateType, setIsCreating, setIsEditing }) =>
 
     return (
         <>
-            <div className='channel-list__container'>
-                <ChannelListContent
-                    setIsCreating={setIsCreating}
-                    setCreateType={setCreateType}
-                    setIsEditing={setIsCreating}
-                />
+            <div className="channel-list__container">
+              <ChannelListContent 
+                setIsCreating={setIsCreating} 
+                setCreateType={setCreateType} 
+                setIsEditing={setIsEditing} 
+              />
             </div>
 
-            <div className='channel-list__container-responsive'
-                style={{ left: toggleContainer ? '0%' : '-89%', backgroundColor: '$005fff' }}
+            <div className="channel-list__container-responsive"
+                style={{ left: toggleContainer ? "0%" : "-89%", backgroundColor: "#005fff"}}
             >
-                <div className='channel-list__container-toggle' onClick={() => setToggleContainer((prevToggleContainer) => !prevToggleContainer)}>
-
+                <div className="channel-list__container-toggle" onClick={() => setToggleContainer((prevToggleContainer) => !prevToggleContainer)}>
                 </div>
-                <ChannelListContent 
-                    setIsCreating={setIsCreating}
-                    setCreateType={setCreateType}
-                    setIsEditing={setIsCreating}
+                    <ChannelListContent 
+                    setIsCreating={setIsCreating} 
+                    setCreateType={setCreateType} 
+                    setIsEditing={setIsEditing}
                     setToggleContainer={setToggleContainer}
                 />
             </div>
--- client/src/components/TeamChannelPreview.jsx
+++ client/src/components/TeamChannelPreview.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Avatar, useChatContext } from 'stream-chat-react';
 
-const TeamChannelPreview = ({ setToggleContainer, channel, type }) => {
+const TeamChannelPreview = ({ setActiveChannel, setToggleContainer, setIsCreating, setIsEditing, channel, type }) => {
     const { channel: activeChannel, client } = useChatContext();
 
     const ChannelPreview = () => (
@@ -32,7 +32,12 @@ const TeamChannelPreview = ({ setToggleContainer, channel, type }) => {
                 : 'channel-preview__wrapper'
         }
         onClick={() => {
-            console.log(channel);
+            setIsCreating(false);
+            setIsEditing(false);
+            setActiveChannel(channel);
+            if(setToggleContainer) {
+                setToggleContainer((prevState) => !prevState)
+            }
         }}
         >
             {type === 'team' ? <ChannelPreview /> : <DirectPreview />}
