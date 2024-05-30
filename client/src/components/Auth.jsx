da2971d2400c08d8d10c8ada2e240d00a239616a
maverickemanuel <mavjamesemanuel@gmail.com>
2024-05-30 14:50:49 -0400

    updated URL for deployment

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -28,7 +28,7 @@ const Auth = () => {
 
         const { username, password, phoneNumber, avatarURL } = form;
 
-        const URL = 'http://localhost:5001/auth';
+        const URL = 'https://messaging-app-b1q0.onrender.com/auth';
 
         const { data : { token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
             username, password, fullName: form.fullName, phoneNumber, avatarURL
